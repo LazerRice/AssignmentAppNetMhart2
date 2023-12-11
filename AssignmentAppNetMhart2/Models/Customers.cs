49fd1d44a65e63ffdc6f679291d5c96ed9e17cf4
Mhart <zcile567@hotmail.com>
2023-12-11 15:15:39 +0100

    Start1

---
+++ AssignmentAppNetMhart2/Models/Customers.cs
@@ -0,0 +1,9 @@
+﻿namespace AssignmentAppNetMhart2.Models
+{
+internal class Customers
+ {
+        public int ID { get; set; }
+
+        public string Name { get; set; } = null
+ }
+
---
+++ AssignmentAppNetMhart2/Serveice/CustmoerService.cs
@@ -0,0 +1,9 @@
+﻿using AssignmentAppNetMhart2.Models;
+
+namespace AssignmentAppNetMhart2.Serveice
+{
+    internal class CustmoerService
+    {
+        private List<Customers>
+    }
+}
