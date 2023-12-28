b76f561dcd1e0f0251d32ed8b6c924433a87e49a
Mhart <zcile567@hotmail.com>
2023-12-28 18:06:15 +0100

    Redo. attempt 3

---
+++ AssignmentAppNetMhart2_Test/CustomerService_Tests.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentAppNetMhart2_Test;
+
+public class CustomerService_Tests
+{
+    [Fact]
+    public void AddToListShould_AddOneCustomerToCustomerList_ThenReturnTrue()
+    {
+        //Arrange 
+    }
+}
